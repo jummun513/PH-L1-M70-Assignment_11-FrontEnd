6515bd55bc3e9929cf07304ded9d964d2adfddde
jummun513 <jummunislam513@gmail.com>
2023-08-18 03:51:58 +0600

    add inventory

---
+++ src/components/pages/Inventory/CarCart/CarCart.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './CarCart.css';
+import { BsFillBookmarkPlusFill } from 'react-icons/bs';
+import { AiFillLike } from 'react-icons/ai';
+
+const CarCart = ({ data, liked, wished }) => {
+    return (
+        <div id='cart' className='p-3 pb-5 mx-3 xsm:mx-0'>
+            <div className="cart-img-container max-w-80">
+                <img className='cart-img max-w-80' src={data.img} alt={data.car + 'car picture'} />
+            </div>
+            <div className="flex flex-col justify-between">
+                <div className="cart-text pt-5 px-3">
+                    <h2 className='text-lg font-semibold text-gray-700'>Manufacturer: {data.car}</h2>
+                    <h3 className='text-base font-semibold text-gray-500 pt-3'>Model: {data.car_model}</h3>
+                    <h3 className='text-base font-semibold text-gray-500 pt-1'>Color: {data.car_color}</h3>
+                    <h3 className='text-base font-semibold text-gray-500 pt-1'>Release Year: {data.car_model_year}</h3>
+                    <h3 className='text-base font-semibold text-gray-500 pt-1'>Price: <span className='text-gray-800 text-lg'>{data.price}</span></h3>
+                </div>
+                <div className='flex justify-between items-center mt-5'>
+                    <div className='flex justify-between items-center'><AiFillLike title='Like' className='h-8 w-8 mr-3 cursor-pointer' fill={liked ? '#88C123' : '#87c12361'}></AiFillLike> <BsFillBookmarkPlusFill title='Add to wish-list.' fill={wished ? '#88C123' : '#87c12361'} className='h-7 w-7 cursor-pointer'></BsFillBookmarkPlusFill></div>
+                    <button type="button" className='btn-style border-2 border-[#88C123] text-white bg-[#88C123] hover:bg-transparent hover:text-[#88C123]'>Buy Now</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default CarCart;
--- src/components/pages/Inventory/Inventory.js
+++ src/components/pages/Inventory/Inventory.js
@@ -1,17 +1,26 @@
 import Navbar from '../../shared/Navbar/Navbar';
 import useFetchData from '../../../hooks/useFetchData';
 import Loading from '../../shared/Loading/Loading';
+import CarCart from './CarCart/CarCart';
+import './Inventory.css';
+import { useState } from 'react';
 
 const Inventory = () => {
     const [cars, loading, setCars] = useFetchData('http://localhost:5000/cars');
+    const [isLiked, setLiked] = useState(false);
+    const [isWished, setWished] = useState(false);
 
     return (
         <div className='svg-container'>
-            <Navbar></Navbar>
-            {
-                loading && <Loading></Loading>
-            }
-            <h1 className='text-5xl top-36 text-white'>This is Inventory - {cars.length}</h1>
+            <div className='relative'><Navbar></Navbar></div>
+            <div className='xxl:max-w-screen-xxl xl:max-w-screen-xl lg:max-w-screen-lg md:max-w-screen-md sm:max-w-screen-sm xsm:max-w-screen-xsm mx-auto relative top-40 md:top-72'>
+                <h1 className='text-[#88C123] text-base sm:text-lg md:text-xl lg:text-2xl xxl:text-5xl text-center'>All Listing Cars</h1>
+                <div id='cart-container' className='gap-7 mt-10 md:mt-20'>
+                    {
+                        loading ? <Loading></Loading> : (cars.map((data, idx) => <CarCart key={idx} data={data} liked={isLiked} wished={isWished}></CarCart>))
+                    }
+                </div>
+            </div>
         </div>
     );
 };
